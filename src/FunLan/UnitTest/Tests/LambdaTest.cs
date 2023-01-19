9773ce32770922f87dd19e555f1929bb4af23d7e
xin-pu <pu.xin@outlook.com>
2023-01-19 15:33:22 +0800

    add lamdba test

---
+++ src/FunLan/UnitTest/Tests/LambdaTest.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using Xunit.Abstractions;
+
+namespace UnitTest.Tests
+{
+    public class LambdaTest : AbstractTest
+    {
+        public LambdaTest(ITestOutputHelper testOutputHelper)
+            : base(testOutputHelper)
+        {
+        }
+
+        [Fact]
+        public void TestFun()
+        {
+            var add = fun((int x, int y) => x + y);
+            var res = add(1, 2);
+            res.Should().Be(3);
+        }
+
+        [Fact]
+        public void TestAct()
+        {
+            var add = act((int x, int y) =>
+            {
+                var d = x + y;
+                Print(d);
+            });
+            add(1, 2);
+        }
+    }
+}
